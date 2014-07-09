30eef0ee78697cc4f06c6e35f823aad63d05ccd3
pavannarne <pavannarne@gmail.com>
2014-07-08 18:42:51 -0700

    commit

--- AirCraftFactory/src/craft/Factory.java
+++ AirCraftFactory/src/craft/Factory.java
@@ -31,11 +31,16 @@ public class Factory
 	{
 		airCraftList = new ArrayList<AirCraft>();	
 		AirCraft airCraft = new Chopper("Apache", 100,200);
+		AirCraft airCraft4 = new Chopper("Apache", 150,200);
+		AirCraft airCraft5 = new Chopper("Apache", 150,250);
+		
 		AirCraft airCraft2 = new Chopper("cathy", 150,200);
 		AirCraft airCraft3 = new Chopper("ethihod", 200,250);
 		airCraftList.add(airCraft);
 		airCraftList.add(airCraft2);
 		airCraftList.add(airCraft3);
+		airCraftList.add(airCraft4);
+		airCraftList.add(airCraft5);
 		
 	}
 
